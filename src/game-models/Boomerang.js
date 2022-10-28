7c1f66d54275d122ad3c6babc8d83e23812963b9
Ignaty_len <ignaty888@mail.ru>
2022-10-28 16:58:04 +0300

    logik1

--- src/game-models/Boomerang.js
+++ src/game-models/Boomerang.js
@@ -1,16 +1,28 @@
 // Бумеранг является оружием.
 // В дальнейшем можно добавить другое оружие.
 // Тогда можно будет создать класс Weapon и воспользоваться наследованием!
+const Game = require('../Game');
+const Enemy = require('./Enemy');
+const Hero = require('./Hero');
 
 class Boomerang {
   constructor() {
     this.skin = '🌀';
-    this.position = 0;
+    this.hero = new Hero();
+    // this.track = new Game();
+    this.enemy = new Enemy();
+    this.position = this.hero.position + 1;
+    this.back = false;
+    this.start = false;
+    this.stop = false;
   }
 
   fly() {
-    this.moveRight();
-    this.moveLeft();
+    if (this.position <= this.enemy.position && this.back === false && this.start === true) {
+      this.moveRight();
+    } else if (this.back === true ) {
+      this.moveLeft();
+    }
   }
 
   moveLeft() {
